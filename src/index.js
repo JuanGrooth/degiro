e66d808e86cb7512014f4d22a6063678fc171fb9
JuanGrooth <62361628+JuanGrooth@users.noreply.github.com>
2020-04-17 21:53:15 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -419,7 +419,7 @@ const create = ({
         )
             .then(res => res.json())
             .then(checkSuccess)
-            .then(json => ({order, confirmationId: json.confirmationId}));
+            .then(json => ({order, confirmationId: json.data.confirmationId}));
     };
 
     /**
@@ -443,7 +443,7 @@ const create = ({
         )
             .then(res => res.json())
             .then(checkSuccess)
-            .then(json => ({orderId: json.orderId}));
+            .then(json => ({orderId: json.data.orderId}));
     };
 
     /**
